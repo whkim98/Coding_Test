0111d11191bba0b18ec0c393d272ab3b4f2bd6e7
김우형 <hyeong7272@daum.net>
2024-11-04 13:36:17 +0900

    2024 11 04

---
+++ src/main/java/com/example/codingtest/CT20241104/Main2.java
@@ -0,0 +1,11 @@
+package com.example.codingtest.CT20241104;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main2 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
